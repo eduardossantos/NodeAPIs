6539d753d9a50ce86f66878b7b7c9d4633b5d594
DESKTOP-NMEDHM2\Icons4u TI <eduardo@icons4u.com.br>
2017-01-11 12:20:32 -0200

    Istalando o mysql.

--- teste-node/app.js
+++ teste-node/app.js
@@ -1,12 +1,8 @@
 // criando o servidor
-var express = require('express');
-var app = express();
-app.set('view engine','ejs');
+var app = require('./config/express')();
+var rotasProdutos = require('./app/routes/produtos')(app);
 // configurando as rotas
-app.get('/produtos',function (request,response) {
 
-    response.render("produtos/lista")
-});
 
 //subindo o servidor
 app.listen(3000,function () {
---
+++ teste-node/config/express.js
@@ -0,0 +1,11 @@
+var app = require('express')();
+app.set('view engine','ejs');
+app.set('views','./app/views')
+module.exports = function () {
+
+    return app;
+
+}
+
+
+
