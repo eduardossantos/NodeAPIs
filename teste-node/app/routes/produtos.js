3a516e095cc691a03b2de0466a8f2a4692603323
DESKTOP-NMEDHM2\Icons4u TI <eduardo@icons4u.com.br>
2017-01-11 20:06:23 -0200

    Gravando dados no banco.

--- teste-node/app/routes/produtos.js
+++ teste-node/app/routes/produtos.js
@@ -2,7 +2,26 @@ module.exports = function (app) {
 
     app.get('/produtos',function (request,response) {
 
-        response.render("produtos/lista")
+        var mysql = require('mysql');
+        //conexão
+        var connection = mysql.createConnection({
+            host:'localhost',
+            user:'root',
+            password:'',
+            database: 'node'
+        });
+        //consulta
+        connection.query("Select * from livros",function (err,result) {
+            response.send(result);
+
+        });
+
+
+
+        connection.end();
+
+
+
     });
 
 }
