b7259acc9de6b81ab0741f714aa26bd9e06bd8e4
DESKTOP-NMEDHM2\Icons4u TI <eduardo@icons4u.com.br>
2017-01-11 11:26:48 -0200

    melhorando página de produtos

--- teste-node/app.js
+++ teste-node/app.js
@@ -1,11 +1,11 @@
 // criando o servidor
 var express = require('express');
 var app = express();
-
+app.set('view engine','ejs');
 // configurando as rotas
 app.get('/produtos',function (request,response) {
 
-    response.send("<html><body><h1>Listagem de Produtos</h1></body></html>")
+    response.render("produtos/lista")
 });
 
 //subindo o servidor
---
+++ teste-node/views/produtos/lista.ejs.js
@@ -0,0 +1,20 @@
+lang;
+"en" >
+    charset;
+"UTF-8" >
+    Title < /title>
+    < /head>
+    < body >
+    id < /td>
+    < td > Nome < /td>
+    < /tr>
+    < tr >
+    1 < /td>
+    < td > Produto;
+do
+    Eduardo < /td>
+        < /tr>
+        < /table>
+        < /body>
+        < /html>;
+while ();
